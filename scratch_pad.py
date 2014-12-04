0fa54467c53d676a72b1d62046c9cbb1daa6a0e0
Steve Falcigno <steve1281@hotmail.com>
2014-12-04 06:52:16 -0500

    finished chapter 5.  Trivial so far.

--- scratch_pad.py
+++ scratch_pad.py
@@ -9,4 +9,52 @@ license()
 # or one time
 help('topics')
 
+exec(open("/Users/steve1281/Desktop/python/withPythonForDummies/Chapter04/FirstApp.py").read())
+#This is a simple Python application.
+exec(open("Chapter04/FirstApp.py").read())
+#This is a simple Python application.
+
+test = 0b100
+test
+#4
+test = 0o100
+test
+#64
+test = 0x100
+test
+#256
+test = 256
+bin(test)
+#'0b100000000'
+oct(test)
+#'0o400'
+hex(test)
+#'0x100'
+
+type(test)
+#<class 'int'>
+
+myStr = str(1234.4)
+type(myStr)
+# <class 'str'>
+myStr
+# '1234.4'
+
+# to bring a module (library?) into the repl, use import
+import datetime
+
+datetime.datetime.now()
+#datetime.datetime(2014, 12, 4, 6, 49, 22, 572848)
+
+
+datetime.datetime.now().date()
+# datetime.date(2014, 12, 4)
+str(datetime.datetime.now().date())
+# '2014-12-04'
+
+
+
+
+
+
 
