196c131d4f18ece7a231554a7bc0804ad80b8b3c
Steve Falcigno <steve1281@hotmail.com>
2014-12-05 15:37:40 -0500

    finished chapter 8. for loop, while loop

--- scratch_pad.py
+++ scratch_pad.py
@@ -199,6 +199,116 @@ else:
 
 
 # chaining ifs
+# see breakfest_menu.py for an example
+
+# chapter 08
+
+# example of for loop
+letternum = 1
+for letter in 'howdy':
+     print ('letter ',letternum, ' is ', letter)
+     letternum += 1
+
+#letter  1  is  h
+#letter  2  is  o
+#letter  3  is  w
+#letter  4  is  d
+#letter  5  is  y
+
+# for loop with break
+
+value = input("type less than 6 characters: ")
+# type less than 6 characters: >? alphabeta
+letternum =1
+ for letter in value:
+     print("letter: ", letternum, " is ", letter)
+     letternum+=1
+     if letternum > 6:
+         print('The string is too long!')
+         break
+
+#letter:  1  is  a
+#letter:  2  is  l
+#letter:  3  is  p
+#letter:  4  is  h
+#letter:  5  is  a
+#letter:  6  is  b
+#The string is too long!
+
+
+# for loop with a continue
+letternum =1
+for letter in 'Howdy!':
+     if letter == 'w':
+         continue
+         print ('envountered w, not processed')
+     print('letter ', letternum, " is ", letter)
+     letternum += 1
+
+#letter  1  is  H
+#letter  2  is  o
+#letter  3  is  d
+#letter  4  is  y
+#letter  5  is  !
+
+## note that print ( encountered ... ) was not processed.
+
+# the while statement
+# (prints out the 10 times table)
+
+x = 1
+y = 1
+print ('{:>4}'.format(' '), end= ' ')
+for x in range(1,11):
+    print ('{:>4}'.format(x), end= ' ')
+
+print()
+
+for x in range(1,11):
+    print ('{:>4}'.format(x), end= ' ')
+    while y <= 10:
+        print ('{:>4}'.format(x * y), end= ' ')
+        y+=1
+    print()
+    y=1
+
+  #      1    2    3    4    5    6    7    8    9   10
+  # 1    1    2    3    4    5    6    7    8    9   10
+  # 2    2    4    6    8   10   12   14   16   18   20
+  # 3    3    6    9   12   15   18   21   24   27   30
+  # 4    4    8   12   16   20   24   28   32   36   40
+  # 5    5   10   15   20   25   30   35   40   45   50
+  # 6    6   12   18   24   30   36   42   48   54   60
+  # 7    7   14   21   28   35   42   49   56   63   70
+  # 8    8   16   24   32   40   48   56   64   72   80
+  # 9    9   18   27   36   45   54   63   72   81   90
+  #10   10   20   30   40   50   60   70   80   90  100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
